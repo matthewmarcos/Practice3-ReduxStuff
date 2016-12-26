cfca5bac14919a65e8b28a4848d39bf3ae6fde8a
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-26 12:12:01 +0800

    Separated store.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
 
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
-
+import store from './store';
 import App from './components/App/App';
-import reducers from './reducers';
 
-const middleware =applyMiddleware(promise(), thunk, logger());
-
-export const store = createStore(
-    reducers,
-    compose(middleware, window.devToolsExtension ? window.devToolsExtension() : f => f)
-);
 
 ReactDOM.render(
     <Provider store={store}>
